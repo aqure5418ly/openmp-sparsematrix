1b1be212b2851474bdbd055f3d5f54f27b8f00c1
monkeycode <monkeycode@monkey.com>
2023-08-07 21:44:44 +0800

    修改了代码中的时间戳位置以发别记录初始化以及乘法所用时间，并修改了.gitgnore

--- AxB/AxB.c
+++ AxB/AxB.c
@@ -4,39 +4,55 @@
 #include <omp.h>
 
 #define SIZE 2048 //矩阵大小
-#define THREADS_NUM 64 //线程数
+#define THREADS_NUM 16 //线程数
 int main() {
     double A[SIZE][SIZE];
     double B[SIZE];
     double C[SIZE];
     double start, end;
 
+    // 为A,B,C数组分配内存并进行内存对齐
+    if (posix_memalign((void**)&B, 32, SIZE * sizeof(double)) != 0 ||
+        posix_memalign((void**)&C, 32, SIZE * sizeof(double)) != 0 ||
+        posix_memalign((void**)&A, 32, SIZE * sizeof(double*)) != 0) 
+    {  
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        if (posix_memalign((void**)&(A[i]), 32, SIZE * sizeof(double)) != 0) {  // Allocate rows for A
+            printf("Memory allocation for A row %d failed\n", i);
+            exit(1);
+        }
+    }
+    
     srand(time(NULL)); // 设置全局随机种子
     omp_set_num_threads(THREADS_NUM);
-start = omp_get_wtime();  // 获取开始时间
-#pragma omp parallel
-{
-    int thread_id = omp_get_thread_num();
-    unsigned int seed = time(NULL) ^ thread_id; // 为每个线程设置不同的种子
-#pragma omp for
-    for (int i = 0; i < SIZE; i++) {
-        B[i] = (double)rand_r(&seed) / (double)RAND_MAX; // 生成0到1之间的随机双精度浮点数
-        for (int j = 0; j < SIZE; j++) {
-            A[i][j] = (double)rand_r(&seed) / (double)RAND_MAX; // 生成0到1之间的随机双精度浮点数
+    start = omp_get_wtime();  // 获取开始时间
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        unsigned int seed = time(NULL) ^ thread_id; // 为每个线程设置不同的种子
+        #pragma omp for
+        for (int i = 0; i < SIZE; i++) {
+            B[i] = (double)rand_r(&seed) / (double)RAND_MAX; // 生成0到1之间的随机双精度浮点数
+            for (int j = 0; j < SIZE; j++) {
+                A[i][j] = (double)rand_r(&seed) / (double)RAND_MAX; // 生成0到1之间的随机双精度浮点数
+            }
         }
     }
 
-#pragma omp for schedule(static)
-    for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                C[j] = A[i][j] * B[j];
+    #pragma omp parallel for schedule(static)
+        for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    C[i] += A[i][j] * B[j];
+                }
             }
-        }
-}
-end = omp_get_wtime();  // 获取结束时间
+    end = omp_get_wtime();  // 获取结束时间
 
     // 测试输出一些元素，看是否初始化成功
-    
+        
     printf("A[0][0] = %f, B[0] = %f\n", A[0][0], B[0]);
     printf("A[2047][2047] = %f, B[2047] = %f\n", A[2047][2047], B[2047]);
     for(int i = 0;i < SIZE;i++)
